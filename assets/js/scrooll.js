3f21c6a92e1227b18733538cff5e29d5158657ba
santiago-144 <carlossantiago.gomezcruz@gmail.com>
2022-04-27 11:49:49 -0500

    clonar pagina web

---
+++ assets/js/scrooll.js
@@ -0,0 +1,4 @@
+ScrollReveal().reveal('#gatito',{delay:500});
+ScrollReveal().reveal('#imagenes',{delay:500});
+ScrollReveal().reveal('#video',{delay:500});
+ScrollReveal().reveal('#adopta',{delay:500});
